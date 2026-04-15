e2d3a44d6146524188a8963ff81b1fb6a081242b
ComicChess <928007032@qq.com>
2026-04-15 22:23:08 +0800

    导入amalgam的spine动画

--- Scripts/Minion/FriendlyAmalgam.cs
+++ Scripts/Minion/FriendlyAmalgam.cs
@@ -9,7 +9,15 @@ public class FriendlyAmalgam : MinionModel
     public override int MaxInitialHp => 1;
     public override int MinInitialHp => 1;
 
-    protected override string VisualsPath => base.VisualsPath;
+    protected override string VisualsPath => "res://TheQueen/scenes/creature_visuals/torch_head_amalgam_minion.tscn";
+
+    public const string IdleAnimName = "idle_loop";
+    public const string DeathAnimName = "die";
+    public const string BuffAnimName = "buff";
+    public const string DebuffAnimName = "_ignore/hug2";
+    public const string AttackAnimName = "attack";
+    public const string PowerAttackAnimName = "debuff";
+    public const string SleepAnimName = "_ignore/string_rigging";
 
     public override Task OnSummon(Player owner, Creature self, MinionSummonOptions options)
     {
