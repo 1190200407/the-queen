666693f7d3be8aebcf6519f208157d89620d0bc9
ComicChess <928007032@qq.com>
2026-05-06 23:39:21 +0800

    把灵魂同调设置为多人卡

--- Scripts/Cards/SoulResonance.cs
+++ Scripts/Cards/SoulResonance.cs
@@ -22,6 +22,7 @@ public sealed class SoulResonance : QueenCardModel
 	private const CardRarity rarity = CardRarity.Rare;
 	private const TargetType targetType = TargetType.Self;
 	private const bool shouldShowInCardLibrary = true;
+	public override CardMultiplayerConstraint MultiplayerConstraint => CardMultiplayerConstraint.MultiplayerOnly;
 
 	protected override IEnumerable<DynamicVar> CanonicalVars =>
 	[
