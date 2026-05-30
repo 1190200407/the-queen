d6ed5f2e001908728f456361e999a3c6d24cf5be
ComicChess <928007032@qq.com>
2026-05-30 19:42:34 +0800

    准备做火堆

--- Scripts/Relics/FirstGiftRelic.cs
+++ Scripts/Relics/FirstGiftRelic.cs
@@ -1,9 +1,9 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using MegaCrit.Sts2.Core.Commands;
-using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Players;
 using MegaCrit.Sts2.Core.Entities.Relics;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models;
@@ -22,11 +22,15 @@ public class FirstGiftRelic : QueenRelicModel
 	protected override IEnumerable<DynamicVar> CanonicalVars => [new CardsVar(1)];
     protected override IEnumerable<IHoverTip> AdditionalHoverTips => [HoverTipFactory.FromPower<SoulLampPower>()];
 
-    public override async Task BeforeCombatStart()
+    public override async Task AfterPlayerTurnStart(PlayerChoiceContext choiceContext, Player player)
 	{
-		// 战斗开始时获得魂灯（SoulLampPower）层数。
+		if (player != base.Owner || player.PlayerCombatState.TurnNumber != 1)
+		{
+			return;
+		}
+
 		int amount = base.DynamicVars["Cards"].IntValue;
-		await QueenCardCmd.AddSoulLamp(new ThrowingPlayerChoiceContext(), base.Owner, amount);
+		await QueenCardCmd.AddSoulLamp(choiceContext, base.Owner, amount);
 	}
 }
 
